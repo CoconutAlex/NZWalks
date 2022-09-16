516485417ebfabb4b8c7a21717b0fc5aa50eb333
CoconutAlex <aakarydas@gmail.com>
2022-09-16 15:21:04 +0300

    End of Section 3

---
+++ NZWalks/NZWalks.API/Models/Profiles/RegionsProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+
+namespace NZWalks.API.Models.Profiles
+{
+    public class RegionsProfile : Profile
+    {
+        public RegionsProfile()
+        {
+            CreateMap<Models.Domain.Region, Models.DTO.Region>()
+                .ReverseMap();
+
+            //If Domain model is different by DTO model
+            //CreateMap<Models.Domain.Region, Models.DTO.Region>()
+            //    .ForMember(dest => dest.Id, options => options.MapFrom(src => src.Id));
+        }
+    }
+}
